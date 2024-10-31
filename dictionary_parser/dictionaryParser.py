4264fbe609cffadee96b3710d845ceb83c0bfca1
Chris Risio <risiochristopher@gmail.com>
2024-10-31 18:42:44 -0400

    fixed y-rule

--- dictionary_parser/dictionaryParser.py
+++ dictionary_parser/dictionaryParser.py
@@ -1,6 +1,7 @@
 import json
 import pronouncing
 import os
+import re
 
 # Get the directory where `dictionaryParser.py` is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -308,6 +309,27 @@ def fszl_check(word, arpabet):
         if phonemes and phonemes[-1] in ["IH", "EH", "AH", "UH", "AA", "AE"]:
             categories["fszl"].append(word)
 
+def is_y_rule_suffix(word):
+    #Maybe temporary Solution
+    exceptions = ["frontier", "glacier", "soldier", "barrier", "carrier", "pier", "priest", "series", "species"]
+    
+    if word in exceptions:
+        return False
+
+    if re.search(r'[^aeiou]ies$', word) or re.search(r'[^aeiou]ied$', word) or re.search(r'[^aeiou]ier$', word) or re.search(r'[^aeiou]iest$', word):
+        return True
+
+    if re.search(r'.*ying$', word):
+        return True
+
+    if re.search(r'[aeiou]y.*$', word):
+        return False  
+
+    if re.search(r'.*ies$', word) and not re.search(r'[aeiou]y', word):
+        return True
+
+    return False
+
 def parse_and_process_words(file_path):
     try:
         with open(file_path, 'r') as file:
@@ -378,6 +400,10 @@ def parse_and_process_words(file_path):
                 vccv(word)
             if len(word) >= 5:
                 vcccv(word)
+                
+            if is_y_rule_suffix(word):
+                categories["y rule suffixes"].append(word)
+
             
             vce_check(word)
             OCE_check(word)
