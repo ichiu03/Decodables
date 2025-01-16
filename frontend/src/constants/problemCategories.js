4c6533baf7bdedfbcfc72bf6a92df41839ec981e
Seamus Sutula <sutulas@bc.edu>
2025-01-16 11:44:20 -0500

    adding updated problem categories

--- frontend/src/constants/problemCategories.js
+++ frontend/src/constants/problemCategories.js
@@ -3,7 +3,7 @@ export const PROBLEM_CATEGORIES = {
       's', 't', 'b', 'm', 'l', 'd', 'n', 'p', 'k', 'j', 'v',
       'z', 'f', 'hard c', 'hard g', 'r', 'h', 'w', 'x', 'y as in yes'
     ],
-    'Column 2 - Vowels': [
+    'Column 2 - All VOWELS (short & long)': [
       'long a', 'short a', 'long i', 'short i', 'long o', 'short o',
       'long u', 'short u', 'long e', 'short e'
     ],
@@ -11,16 +11,19 @@ export const PROBLEM_CATEGORIES = {
       'fszl', 'qu', 'sh', 'ay', 'ck', 'ee', 'ch', 'or', 's blends',
       'l blends', 'r blends', '-ing, -ong, -ang, -ung', 'all', 'th', 'oy',
       '-ink, -ank, -onk, -unk', '-ft, -nd, -st', '-sp, -nt, -mp', '-sk, -lt, -lk', '-ct, -pt',
-      'y as in dry', 'ar', 'wh', 'oo as in school', 'oo as in book', 'vce',
-      'er', 'ow as in plow', 'ow as in snow', 'vccv', 'Open syll.', 'Closed syll.', 'contractions'
+      'y as in dry', 'ar', 'wh', 'oo as in school', 'oo as in book',
+      '1 syll. vce', '2 syll. vce', '2 syll. vce w/ closed syllable', '3 syll. vce w/ closed syllables',
+      'er', 'ow as in plow', 'ow as in snow', '1 syll. open', '2 syll. open', '2 syll. closed w/ closed',
+      '2 syll. open w/ silent e', 'multisyllable open syllable words w/ v/v pattern (not finished)', 'multisyllable mixed with closed, silent e, open (not finished)',
+      '1 syll. closed', '2 syll. closed (VCV, VCCV, VCCCV)', 'contractions', '3 syll. closed'
     ],
     'Column 4': [
       'ear as in hear', 'ear as in early', 'y as in bumpy', 'aw', 'ly', 'ea as in eat',
-      'ea as in bread', '3-letter beg. blends', 'vcv', 'vcccv', 'tch', 'soft c',
-      'soft g', 'ai', 'igh', 'ed', '-ble, -cle, -dle, -fle, -gle, -kle, -ple, -tle, -zle',
-      'vrl syllables', 'oa', 'ir', '-ild, -ind, -old, -ost', 'oi', 'double rule-suffixes',
-      'ew as in few/blew', 'v/v pattern', 'kn', 'e rule-suffixes', 'ou as in south', 'ur',
-      'dge', 'y rule suffixes', 'tion'
+      'ea as in bread', '3-letter beg. blends', 'tch', 'soft c',
+      '-ble, -cle, -dle, -fle, -gle, -kle, -ple, -tle, -zle', 'soft g', 'ai', 'igh', 'ed',
+      'cle ending', 'vowel_team', 'r-controlled', 'oa', 'ir', '-ild, -ind, -old, -ost',
+      'oi', 'double rule-suffixes', 'ew as in few/blew', 'v/v pattern', 'kn', 'e rule-suffixes',
+      'ou as in south', 'ur', 'dge', 'y rule suffixes', 'tion'
     ],
     'Column 5': [
       'au', 'war', 'ey as in monkey', 'ey as in they', 'ph',
